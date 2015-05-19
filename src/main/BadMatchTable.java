3f2670e4e672de16e41f1b3759f1579beacde7ea
Nicolas LECOURTOIS <ln404361@etu.unice.fr>
2015-05-19 12:14:42 +0200

    Ajout BadMatchTable

--- src/main/BadMatchTable.java
+++ src/main/BadMatchTable.java
@@ -1,11 +1,31 @@
 package main;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class BadMatchTable {
-    Map<Character, Integer> mapTable;
+    private Map<Character, Integer> mapTable;
+    private int length;
 
     public BadMatchTable(String subString) {
+        length = subString.length();
+        mapTable = new HashMap<>();
+        for (int i = 0; i < subString.length() - 1; i++) {
+            mapTable.put(subString.charAt(i), subString.length() - 1 - i);
+        }
+    }
 
+    public int getShift(char c) {
+        return mapTable.getOrDefault(c, length);
+    }
+
+    @Override
+    public String toString() {
+        return mapTable.toString();
+    }
+
+    public static void main(String[] args) {
+        BadMatchTable bmt = new BadMatchTable("aaa");
+        System.out.println(bmt);
     }
 }
