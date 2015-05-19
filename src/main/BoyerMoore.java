1ba9cafb7ccbb2b3cebfbfb063ca9bc2e656bfe0
Alexandre CAZALA <alexandre.cazala@gmail.com>
2015-05-19 11:18:02 +0200

    Take

--- src/main/BoyerMoore.java
+++ src/main/BoyerMoore.java
@@ -1,12 +1,16 @@
 package main;
 
+import java.util.HashMap;
+
 public class BoyerMoore {
 
 	private String text;
 	private String toSearch;
+	private HashMap<String, Integer> decalTable;
 	public BoyerMoore(String text, String toSearch) {
 		this.text = text;
 		this.toSearch = toSearch;
+		decalTable = new HashMap<>();
 	}
 	
 	public void decal (int i) {
